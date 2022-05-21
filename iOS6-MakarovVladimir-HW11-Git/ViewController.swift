c39737afb25ea6fd1a37174afa75c4ca0a6d2204
Vladimir Makarov <gerbilin@yandex.ru>
2022-05-22 00:34:47 +0300

    Initial Commit

---
+++ iOS6-MakarovVladimir-HW11-Git/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-MakarovVladimir-HW11-Git
+//
+//  Created by Gerbilin Mac on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
